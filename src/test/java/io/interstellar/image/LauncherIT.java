fc4630e7e3a8fd0c738fe2e815c84135074a3dce
Alexey Venderov <alexey.venderov@n26.com>
2019-03-02 19:01:27 +0100

    Base project structure

---
+++ src/test/java/io/interstellar/image/LauncherIT.java
@@ -0,0 +1,13 @@
+package io.interstellar.image;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LauncherIT {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
